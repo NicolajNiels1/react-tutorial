af3b8e392de8269123bc80fb2f39561d683c7fed
Nicolaj <nicolaj.nielsen@isfit.no>
2019-04-03 01:17:51 +0200

    done with section 2, lecture 18

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import ReactDOM from "react-dom";
 
 // Creat a React component
 const App = function() {
-  return <div>Hi There!</div>;
+  return (
+    <div>
+      <label class="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        Submit
+      </button>
+    </div>
+  );
 };
 // Take the React component and shot it on the screen
 ReactDOM.render(<App />, document.querySelector("#root"));
