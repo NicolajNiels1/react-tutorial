1e0fa0793b2d1635fb27f756bba8591d49085b65
Nicolaj <nicolaj.nielsen@isfit.no>
2019-04-05 16:58:58 +0200

    Made changes to structure of git repo

--- jsx-tutorial/src/index.js
+++ jsx-tutorial/src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Creat a React component
+const App = () => {
+  const buttonText = "Click me!";
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText}
+      </button>
+    </div>
+  );
+};
+// Take the React component and shot it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
